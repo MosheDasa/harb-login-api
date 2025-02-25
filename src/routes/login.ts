5a0186817d1425d15674ff47358bfee7acfcb94b
Barhon Moshe Dasa <moshebdasa@gmail.com>
2025-02-25 06:51:04 +0200

    dfd

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,9 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { chromium } from "playwright";
 import { LogingController } from "../controller/loging-controller";
 
 export async function loginRoute(fastify: FastifyInstance) {
-  fastify.get("/login", async (request, reply) => {
+  fastify.get("/api/login", async (request, reply) => {
     const result = await LogingController.login();
     return result;
   });
